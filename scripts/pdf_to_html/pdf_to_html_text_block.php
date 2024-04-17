ebc1739a543fc798f64e5936fe6d5bf117c480ff
echtyushi <ja.agripo2003@gmail.com>
2024-04-17 14:44:07 +0200

    Testing text block

--- scripts/pdf_to_html/pdf_to_html_text_block.php
+++ scripts/pdf_to_html/pdf_to_html_text_block.php
@@ -2,34 +2,33 @@
 
 class pdf_to_html_text_block
 {
-    static private  $arrayBlocks =                  [];
-    static private  $maxTextYSeparator =            5; //top position 
+    static private $arrayBlocks = [];
+    static private $maxTextYSeparator = 5; //top position
 
     //#####################################################################
     static private function returnArrayTextPropertyWithLinkedMultipleIndexes($pageObj, $prop)
-    {	
+    {
         $propValues = [];
 
-        foreach ($pageObj as $index => $item) 
-        {
-            if($item['tag'] === "image") { continue; }
+        foreach ($pageObj as $index => $item) {
+            if ($item['tag'] === "image") {
+                continue;
+            }
             $value = (string)$item[$prop];
-            if (!isset($propValues[$value])) {$propValues[$value] = [];}
+            if (!isset($propValues[$value])) {
+                $propValues[$value] = [];
+            }
             $propValues[$value][] = $index;
         }
 
-        foreach ($propValues as $prop => $arr) 
-        {
-            
-                if( sizeof($arr) <= 1 ) 
-                {   
-                    unset($propValues[$prop]); 
-                } 
-                else 
-                {
-                    sort($propValues[$prop], SORT_NUMERIC);
-                }
-         
+        foreach ($propValues as $prop => $arr) {
+
+            if (sizeof($arr) <= 1) {
+                unset($propValues[$prop]);
+            } else {
+                sort($propValues[$prop], SORT_NUMERIC);
+            }
+
         }
 
         ksort($propValues);
@@ -38,22 +37,24 @@ class pdf_to_html_text_block
 
     //#####################################################################
     static public function process($page)
-    {	
-            
-        $obj = digi_pdf_to_html::$arrayPages[$page]['content'];
-        $len = sizeof( $obj );  
-        $arr = self::returnArrayTextPropertyWithLinkedMultipleIndexes( $obj ,"left" );
+    {
 
-        print_r($obj);exit;
-        print_r($arr);exit;
+        $obj = digi_pdf_to_html::$arrayPages[$page]['content'];
+        $len = sizeof($obj);
+        $arr = self::returnArrayTextPropertyWithLinkedMultipleIndexes($obj, "left");
+
+        print_r($obj);
+        exit;
+        print_r($arr);
+        exit;
 
         $leftValues = [];
-        foreach ($obj as $index => $item) 
-        {
-            if($item['tag'] === "image") {continue;}
+        foreach ($obj as $index => $item) {
+            if ($item['tag'] === "image") {
+                continue;
+            }
             $left = $item['left'];
-            if (!isset($leftValues[$left])) 
-            {
+            if (!isset($leftValues[$left])) {
                 $leftValues[$left] = [];
             }
             $leftValues[$left][] = $index;
